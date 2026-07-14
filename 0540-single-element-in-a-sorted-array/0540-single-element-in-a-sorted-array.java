a8ff5781d3eead11a4232d772a3683bcd7694f53
Himanshu <khsiwan510@gmail.com>
2026-07-14 10:08:32 +0530

    Time: 0 ms (100%), Space: 53 MB (21.14%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -4,8 +4,18 @@ class Solution {
         if(n == 1) return nums[0];
         if(nums[0] != nums[1]) return nums[0];
         if(nums[n-1] != nums[n-2]) return nums[n-1];
-        for(int i = 1; i < n-1; i++){
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) return nums[i];
+        // Binary search approach
+        int lo = 0, hi = n-1;
+        while(lo<=hi){
+            int mid = lo + (hi - lo)/2;
+            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]) return nums[mid];
+            int firstIndex = mid, secIndex = mid;
+            if(nums[mid] == nums[mid-1]) firstIndex = mid-1;
+            else secIndex = mid+1;
+            int leftCount = firstIndex - lo;
+            int rightCount = hi - secIndex;
+            if(leftCount % 2 == 0) lo = secIndex+1;
+            else hi = firstIndex-1;            
         }
         return -1;
         
