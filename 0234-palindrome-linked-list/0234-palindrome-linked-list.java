9a486c046947a88f8f3a02669eda0e8f34583f5b
Himanshu <khsiwan510@gmail.com>
2026-08-17 10:21:41 +0530

    Time: 6 ms (34.95%), Space: 101.3 MB (12.26%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        ListNode temp = head;
+        while(temp!=null){
+            ans.add(temp.val);
+            temp = temp.next;
+        }
+        int i = 0, j = ans.size()-1;
+        while(i<=j){
+            if(ans.get(i) != ans.get(j)) return false;
+            i++; j--;
+        }
+        return true;
+    }
+}
