dbd9e6001a00ff0de4fa51424933f4dcaa5ad8b6
Himanshu <khsiwan510@gmail.com>
2026-08-17 10:48:46 +0530

    Time: 4 ms (66.64%), Space: 94.3 MB (80.05%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -1,16 +1,50 @@
 class Solution {
-    public boolean isPalindrome(ListNode head) {
-        ArrayList<Integer> ans = new ArrayList<>();
-        ListNode temp = head;
-        while(temp!=null){
-            ans.add(temp.val);
-            temp = temp.next;
+    public ListNode reverse(ListNode head){
+        ListNode curr = head;
+        ListNode forwd = null;
+        ListNode prev = null;
+        while(curr!=null){
+            forwd = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = forwd;
         }
-        int i = 0, j = ans.size()-1;
-        while(i<j){
-            if(ans.get(i) != ans.get(j)) return false;
-            i++; j--;
+        return prev;
+    }
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        // Break List into two list
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode head2 = slow.next;
+        slow.next = null;
+
+        // Reverse List2 
+        head2 = reverse(head2);
+
+        // compare to get answer
+        while(head2!=null){
+            if(head.val != head2.val) return false;
+            head = head.next;
+            head2 = head2.next;
         }
         return true;
+
+
+        // ArrayList<Integer> ans = new ArrayList<>();
+        // ListNode temp = head;
+        // while(temp!=null){
+        //     ans.add(temp.val);
+        //     temp = temp.next;
+        // }
+        // int i = 0, j = ans.size()-1;
+        // while(i<j){
+        //     if(ans.get(i) != ans.get(j)) return false;
+        //     i++; j--;
+        // }
+        // return true;
     }
 }
