b564ca2fa7066216c8402da9f56d296a9bf92e77
Himanshu <khsiwan510@gmail.com>
2026-08-19 12:24:20 +0530

    Time: 0 ms (100%), Space: 42.8 MB (72.08%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -1,50 +1,46 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public void addPre(TreeNode root, List<Integer> ans){
-        if(root == null){
-            ans.add(100000);
-            return;
-        }
-        ans.add(root.val);
-        addPre(root.left, ans);
-        addPre(root.right, ans);
-    }
-    public void addPost(TreeNode root, List<Integer> ans){
-        if(root == null){
-            ans.add(100000);
-            return;
-        }        
-        addPost(root.right, ans);
-        addPost(root.left, ans);
-        ans.add(root.val);
-    }
-    public boolean isSameTree(TreeNode p, TreeNode q) {
-        List<Integer> ans = new ArrayList<>();
-        addPre(p, ans);
-        addPost(q, ans);
-        int i = 0, j = ans.size()-1;
-        while(i<j){
-            if(ans.get(i).equals(ans.get(j))){
-                i++; j--;
-                continue;
-            }
-            else return false;            
-        }
+class Solution{
+    public boolean isSameTree(TreeNode p, TreeNode q){
+        if(p == null && q == null) return true;
+        if(p == null || q == null) return false;
+        if(p.val != q.val) return false;
+        if(!isSameTree(p.left, q.left)) return false;
+        if(!isSameTree(p.right, q.right)) return false;
         return true;
-        
     }
 }
+//    M - 1: ArrayList
+// class Solution {
+//     public void addPre(TreeNode root, List<Integer> ans){
+//         if(root == null){
+//             ans.add(100000);
+//             return;
+//         }
+//         ans.add(root.val);
+//         addPre(root.left, ans);
+//         addPre(root.right, ans);
+//     }
+//     public void addPost(TreeNode root, List<Integer> ans){
+//         if(root == null){
+//             ans.add(100000);
+//             return;
+//         }        
+//         addPost(root.right, ans);
+//         addPost(root.left, ans);
+//         ans.add(root.val);
+//     }
+//     public boolean isSameTree(TreeNode p, TreeNode q) {
+//         List<Integer> ans = new ArrayList<>();
+//         addPre(p, ans);
+//         addPost(q, ans);
+//         int i = 0, j = ans.size()-1;
+//         while(i<j){
+//             if(ans.get(i).equals(ans.get(j))){
+//                 i++; j--;
+//                 continue;
+//             }
+//             else return false;            
+//         }
+//         return true;
+        
+//     }
+// }
