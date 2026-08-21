695d05a99e303e2ee450356cdaea7201018acfd9
Himanshu <khsiwan510@gmail.com>
2026-08-21 09:48:47 +0530

    Time: 0 ms (100%), Space: 45.8 MB (10.01%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -1,12 +1,29 @@
 class Solution {
+    static boolean flag;
     public int levels(TreeNode root){
-        return (root == null) ? 0 : (1 + Math.max(levels(root.left), levels(root.right)));
+        if(root == null) return 0;
+        int leftLevels = levels(root.left);
+        int rightLevels = levels(root.right);
+        if(Math.abs(leftLevels - rightLevels) > 1 ) flag = false;
+        return 1 + Math.max(leftLevels, rightLevels);
     }
     public boolean isBalanced(TreeNode root) {
         if(root == null) return true;
-        int leftLevels = levels(root.left);
-        int rightLevels = levels(root.right);
-        if(Math.abs(leftLevels - rightLevels) > 1) return false;
-        return isBalanced(root.left) && isBalanced(root.right);        
+        flag = true;
+        levels(root);
+        return flag;
     }
 }
+
+// class Solution {
+//     public int levels(TreeNode root){
+//         return (root == null) ? 0 : (1 + Math.max(levels(root.left), levels(root.right)));
+//     }
+//     public boolean isBalanced(TreeNode root) {
+//         if(root == null) return true;
+//         int leftLevels = levels(root.left);
+//         int rightLevels = levels(root.right);
+//         if(Math.abs(leftLevels - rightLevels) > 1) return false;
+//         return isBalanced(root.left) && isBalanced(root.right);        
+//     }
+// }
