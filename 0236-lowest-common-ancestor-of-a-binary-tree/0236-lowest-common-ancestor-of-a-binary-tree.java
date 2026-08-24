541342e6775934fa2a4fe13000d987c3bb6f0b7a
Himanshu <khsiwan510@gmail.com>
2026-08-24 17:53:13 +0530

    Time: 16 ms (52.31%), Space: 70.1 MB (8.61%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -1,16 +1,25 @@
 class Solution {
-    public boolean exits(TreeNode root, int val){
-        if(root == null) return false;
-        if(root.val == val) return true;
-        return exits(root.left, val) || exits(root.right, val);
-    }
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null ) return null;
-        if(root.val == p.val || root.val == q.val) return root;
-        boolean pLiesInLeft = exits(root.left, p.val);
-        boolean qLiesInLeft = exits(root.left, q.val);
-        if(pLiesInLeft && qLiesInLeft) return lowestCommonAncestor(root.left, p, q);
-        else if(!pLiesInLeft && !qLiesInLeft) return lowestCommonAncestor(root.right, p, q);
-        else return root;
+        if(root == null || root.val == p.val || root.val == q.val) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if(left != null && right != null) return root;
+        return (left != null) ? left : right;
     }
 }
+// class Solution {
+//     public boolean exits(TreeNode root, int val){
+//         if(root == null) return false;
+//         if(root.val == val) return true;
+//         return exits(root.left, val) || exits(root.right, val);
+//     }
+//     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//         if(root == null ) return null;
+//         if(root.val == p.val || root.val == q.val) return root;
+//         boolean pLiesInLeft = exits(root.left, p.val);
+//         boolean qLiesInLeft = exits(root.left, q.val);
+//         if(pLiesInLeft && qLiesInLeft) return lowestCommonAncestor(root.left, p, q);
+//         else if(!pLiesInLeft && !qLiesInLeft) return lowestCommonAncestor(root.right, p, q);
+//         else return root;
+//     }
+// }
