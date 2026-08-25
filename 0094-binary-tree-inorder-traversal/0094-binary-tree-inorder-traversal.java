dcdb1c588982c3e3de8d4c6465177ff0b5ed269d
Himanshu <khsiwan510@gmail.com>
2026-08-25 12:11:25 +0530

    Time: 1 ms (6.31%), Space: 43.1 MB (70.83%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -1,28 +1,32 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    public void inOrderRecursive(TreeNode root, List<Integer> ans){
-        if(root == null) return;
-        inOrderRecursive(root.left, ans);
-        ans.add(root.val);
-        inOrderRecursive(root.right, ans);
-    }
+    // public void inOrderRecursive(TreeNode root, List<Integer> ans){
+    //     if(root == null) return;
+    //     inOrderRecursive(root.left, ans);
+    //     ans.add(root.val);
+    //     inOrderRecursive(root.right, ans);
+    // }
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
-        inOrderRecursive(root, ans);
+        // inOrderRecursive(root, ans);
+        Stack<TreeNode> st = new Stack<>();
+        TreeNode curr = root;
+        while(st.size() > 0 || curr != null){
+            if(curr != null){
+                if(curr.left != null){
+                    st.push(curr);
+                    curr = curr.left;
+                }
+                else{ // curr.left == null
+                    ans.add(curr.val);
+                    curr = curr.right;
+                }
+            }
+            else{
+                TreeNode top = st.pop();
+                ans.add(top.val);
+                curr = top.right;
+            }
+        }
         return ans;
     }
 }
