72a1da9addfb3be20cd88facd902fe3a56a01c78
Himanshu <khsiwan510@gmail.com>
2026-08-25 10:39:40 +0530

    Time: 0 ms (100%), Space: 43.9 MB (53.31%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -1,22 +1,41 @@
 class Solution {
-    public void preOrderTraversal(TreeNode root, ArrayList<TreeNode> ans){
-        if(root == null) return;
-        ans.add(root);
-        preOrderTraversal(root.left, ans);
-        preOrderTraversal(root.right, ans);
-    }
     public void flatten(TreeNode root) {
         if(root == null) return;
-        ArrayList<TreeNode> ans = new ArrayList<>();
-        preOrderTraversal(root, ans);
-        TreeNode curr = root;
-        int i = 1;
-        while(i < ans.size()){
-            curr.left = null;
-            curr.right = ans.get(i);
-            curr = curr.right;
-            i++;
+        TreeNode lst = root.left; // left subtree
+        TreeNode rst = root.right;
+        root.left = null;
+        root.right = null;
+        flatten(lst);
+        flatten(rst);
+        root.right = lst;
+        TreeNode last = root; // yeh lst ka last element hoga
+        while(last.right != null){
+            last = last.right;
         }
-        curr.right = null;
+        last.right = rst;
+
     }
+
+
+    // // A.S => O(n) recursive height => O(h);
+    // public void preOrderTraversal(TreeNode root, ArrayList<TreeNode> ans){
+    //     if(root == null) return;
+    //     ans.add(root);
+    //     preOrderTraversal(root.left, ans);
+    //     preOrderTraversal(root.right, ans);
+    // }
+    // public void flatten(TreeNode root) {
+    //     if(root == null) return;
+    //     ArrayList<TreeNode> ans = new ArrayList<>();
+    //     preOrderTraversal(root, ans);
+    //     TreeNode curr = root;
+    //     int i = 1;
+    //     while(i < ans.size()){
+    //         curr.left = null;
+    //         curr.right = ans.get(i);
+    //         curr = curr.right;
+    //         i++;
+    //     }
+    //     curr.right = null;
+    // }
 }
