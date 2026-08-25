d6aef363ba4a9268e1e63d8bce0013a488f1103a
Himanshu <khsiwan510@gmail.com>
2026-08-25 11:49:07 +0530

    Time: 1 ms (11.96%), Space: 43.3 MB (15.24%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -1,28 +1,33 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    public void postOrderRecursive(TreeNode root, List<Integer> ans){
-        if(root == null) return;
-        postOrderRecursive(root.left, ans);
-        postOrderRecursive(root.right, ans);
-        ans.add(root.val);
-    }
+    // public void postOrderRecursive(TreeNode root, List<Integer> ans){
+    //     if(root == null) return;
+    //     postOrderRecursive(root.left, ans);
+    //     postOrderRecursive(root.right, ans);
+    //     ans.add(root.val);
+    // }
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
-        postOrderRecursive(root, ans);
-        return ans;
+        // postOrderRecursive(root, ans);
+        // Iterative 
+        if(root == null) return ans;        
+        else{
+            Stack<TreeNode> st = new Stack<>();
+            st.push(root);
+            while(st.size() > 0){
+                TreeNode top = st.pop();
+                ans.add(top.val);
+                if(top.left != null) st.push(top.left);
+                if(top.right != null) st.push(top.right);
+            }
+            int i = 0, j = ans.size()-1;
+            while(i<j){
+                int temp = ans.get(i);
+                ans.set(i,ans.get(j));
+                ans.set(j,temp);
+                i++;
+                j--;
+            }
+            return ans;
+        }
     }
 }
