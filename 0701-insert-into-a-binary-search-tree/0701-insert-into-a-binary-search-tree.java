97dfeeb19d48f3705f80b0c5a57c2546320adbf6
Himanshu <khsiwan510@gmail.com>
2026-08-28 14:46:38 +0530

    Time: 0 ms (100%), Space: 46.8 MB (94.08%) - LeetHub

--- 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.java
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.java
@@ -1,20 +1,26 @@
 class Solution {
-    void attach(TreeNode root, int val){
-        if(root == null) return;
-        if(root.val < val){
-            if(root.right == null) root.right = new TreeNode(val);
-            else // root.right != null 
-                attach(root.right, val);
-        }
-        else{ // root.val > val
-            if(root.left == null) root.left = new TreeNode(val);
-            else // root.left != null
-                attach(root.left, val);
-        }
-    }
     public TreeNode insertIntoBST(TreeNode root, int val) {
         if(root == null) return new TreeNode(val);
-        attach(root, val);
+        if(root.val < val) root.right = insertIntoBST(root.right, val);
+        else root.left = insertIntoBST(root.left, val);
         return root;
     }
+    // void attach(TreeNode root, int val){
+    //     if(root == null) return;
+    //     if(root.val < val){
+    //         if(root.right == null) root.right = new TreeNode(val);
+    //         else // root.right != null 
+    //             attach(root.right, val);
+    //     }
+    //     else{ // root.val > val
+    //         if(root.left == null) root.left = new TreeNode(val);
+    //         else // root.left != null
+    //             attach(root.left, val);
+    //     }
+    // }
+    // public TreeNode insertIntoBST(TreeNode root, int val) {
+    //     if(root == null) return new TreeNode(val);
+    //     attach(root, val);
+    //     return root;
+    // }
 }
