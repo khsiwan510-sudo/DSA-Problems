b8ef060115996cd2aa3b54b3a3ae784f4b1284df
Himanshu <khsiwan510@gmail.com>
2026-08-28 15:21:22 +0530

    Time: 0 ms (100%), Space: 46.5 MB (89.72%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -1,13 +1,29 @@
 class Solution {
-    private void inorderTraversal(TreeNode root, ArrayList<Integer> ans){
+    void inorder(TreeNode root, int[] ans){
         if(root == null) return;
-        inorderTraversal(root.left, ans);
-        ans.add(root.val);
-        inorderTraversal(root.right, ans);
+        inorder(root.left, ans);
+        ans[0]--;
+        if(ans[0]==0) ans[1] = root.val;
+        inorder(root.right, ans);
+        
     }
     public int kthSmallest(TreeNode root, int k) {
-        ArrayList<Integer> ans = new ArrayList<>();
-        inorderTraversal(root, ans);
-        return ans.get(k-1);        
+        int[] req = new int[2];
+        req[0] = k;
+        req[1] = -1; // if K is greater than tree length
+        inorder(root, req);
+        return req[1];
     }
+    // //inorderTraversal of BST is always in sorted order;
+    // private void inorderTraversal(TreeNode root, ArrayList<Integer> ans){
+    //     if(root == null) return;
+    //     inorderTraversal(root.left, ans);
+    //     ans.add(root.val);
+    //     inorderTraversal(root.right, ans);
+    // }
+    // public int kthSmallest(TreeNode root, int k) {
+    //     ArrayList<Integer> ans = new ArrayList<>();
+    //     inorderTraversal(root, ans);
+    //     return ans.get(k-1);        
+    // }
 }
