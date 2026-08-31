3d253c1dbabb592f7c9311d51a542613bce57a2c
Himanshu <khsiwan510@gmail.com>
2026-08-31 19:07:03 +0530

    Time: 1 ms (16.62%), Space: 46.6 MB (5.25%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -1,25 +1,53 @@
-class Pair {
+class Triplet {
     long max;
     long min;
-    Pair(long max, long min){
+    boolean isBST;
+    Triplet(long max, long min, boolean isBST){
         this.max = max;
         this.min = min;
+        this.isBST = isBST;
     }
 }
 class Solution {
-    public Pair maxMin(TreeNode root, boolean[] flag){
-        if(root == null) return new Pair(Long.MIN_VALUE, Long.MAX_VALUE);
-        Pair lst = maxMin(root.left, flag);
-        Pair rst = maxMin(root.right, flag);
+    public Triplet maxMin(TreeNode root){
+        if(root == null) return new Triplet(Long.MIN_VALUE, Long.MAX_VALUE, true);
+        Triplet lst = maxMin(root.left);
+        Triplet rst = maxMin(root.right);
         long val = root.val;
-        long mx = Math.max(root.val, Math.max(lst.max, rst.max));
-        long mn = Math.min(root.val, Math.min(lst.min, rst.min));
-        if(lst.max >= val || rst.min <= val) flag[0] = false;
-        return new Pair(mx, mn);
+        long max = Math.max(val, Math.max(lst.max, rst.max));
+        long min = Math.min(val, Math.min(lst.min, rst.min));
+        boolean isBST = (val > lst.max) && (val < rst.min) && lst.isBST && rst.isBST;
+        return new Triplet(max, min, isBST);
     }
-    public boolean isValidBST(TreeNode root) {
-        boolean[] flag = {true};
-        maxMin(root, flag);
-        return flag[0];
+    public boolean isValidBST(TreeNode root){
+        return maxMin(root).isBST;
     }
 }
+
+
+
+// class Pair {
+//     long max;
+//     long min;
+//     Pair(long max, long min){
+//         this.max = max;
+//         this.min = min;
+//     }
+// }
+// class Solution {
+//     public Pair maxMin(TreeNode root, boolean[] flag){
+//         if(root == null) return new Pair(Long.MIN_VALUE, Long.MAX_VALUE);
+//         Pair lst = maxMin(root.left, flag);
+//         Pair rst = maxMin(root.right, flag);
+//         long val = root.val;
+//         long mx = Math.max(root.val, Math.max(lst.max, rst.max));
+//         long mn = Math.min(root.val, Math.min(lst.min, rst.min));
+//         if(lst.max >= val || rst.min <= val) flag[0] = false;
+//         return new Pair(mx, mn);
+//     }
+//     public boolean isValidBST(TreeNode root) {
+//         boolean[] flag = {true};
+//         maxMin(root, flag);
+//         return flag[0];
+//     }
+// }
